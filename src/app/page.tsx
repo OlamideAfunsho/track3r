2ac2b7a8f19be1943d9664b0bf787b99d6cf787c
Afunsho Olamide <abolamide77@gmail.com>
2025-10-06 12:36:38 +0100

    update landing page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Navbar from "../../components/Navbar";
 export default function Home() {
   return (
     <div className="">
-      <main className="bg-[#EAEAEC] w-full px-12 py-4">
+      <main className="bg-[#EAEAEC] w-full px-6 sm:px-12 py-4">
         <Navbar />
       </main>
     </div>

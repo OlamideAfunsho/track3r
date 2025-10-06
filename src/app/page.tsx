ccc975dadbadb5fa277f1e90eb5edee8718a4802
Afunsho Olamide <abolamide77@gmail.com>
2025-10-06 13:04:34 +0100

    update landing page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Hero from "../../components/Hero";
 import Navbar from "../../components/Navbar";
 
 
@@ -6,6 +7,7 @@ export default function Home() {
     <div className="">
       <main className="bg-[#EAEAEC] w-full px-6 sm:px-12 py-4">
         <Navbar />
+        <Hero />
       </main>
     </div>
   );

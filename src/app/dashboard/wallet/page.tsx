0ade1dbd829f7ca1fdf1aa9888d22ce549570c41
Afunsho Olamide <abolamide77@gmail.com>
2025-11-22 13:03:36 +0100

    Fixed wallet page layout style

--- src/app/dashboard/wallet/page.tsx
+++ src/app/dashboard/wallet/page.tsx
@@ -6,11 +6,11 @@ import Image from 'next/image'
 
 const page = () => {
   return (
-    <div className='px-4 py-6'>
+    <div className='px-4 py-6 h-[400px]'>
       <h1 className={`${afacad.className} flex items-center gap-2 md:text-left text-2xl mb-2`}>Wallet <span className='text-[#544DF2]'>Page</span><Image src={walletIcon} alt='wallet-icon' /></h1>
       <p className={`${afacad.className} text-[#667085] mb-2`}>Your wallet will let you deposit, withdraw, and track transactions securely.</p>
 
-      <button className='bg-[#544DF2] text-[14px] text-[#FFFFFF] px-[16px] py-[6px] rounded-[8px] right-0 hover:bg-[rgba(84,77,242,0.8)] shadow-[inset_0px_4px_11.2px_0px_#FAFAFAA1]'>
+      <button className='bg-[#544DF2] text-[14px] text-[#FFFFFF] px-4 py-1.5 rounded-[8px] right-0 hover:bg-[rgba(84,77,242,0.8)] shadow-[inset_0px_4px_11.2px_0px_#FAFAFAA1]'>
                   Add Funds (Coming soon)
       </button>
       
